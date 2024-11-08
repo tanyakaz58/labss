bd601742ab8f38e7693194a2dcc1e19e9cef5059
tanyakaz58 <tanyakazakova58ru@gmail.com>
2024-11-08 13:55:11 +0300

    Add files via upload

---
+++ lab4/Номера заданий.py
@@ -0,0 +1,7 @@
+import random
+random.seed (411)
+x1=random.randint (1,33)
+x2=random.randint (1,9)
+x3=random.randint (1,14)
+print (x1, '- номер 1-ого уровня; ',x2, '- номер 2-ого уровня;',x3, '- номер 3-ого уровня')
+print ()
